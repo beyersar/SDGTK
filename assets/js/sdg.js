d2c85fb3c717f0d0722e465c783601a28daeda18
tt053626 <79691995+tt053626@users.noreply.github.com>
2021-08-11 20:34:17 +0000

    Deploying to gh-pages from  @ 244b55760d4d75f2ee31c8322836bcee29e59b30 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2685,8 +2685,8 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"tileURL":"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"attribution":"<a href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\">&copy; OpenStreetMap contributors</a>"}},
-      mapLayers: [{"min_zoom":5,"max_zoom":20,"subfolder":"districts","label":"indicator.map"}],
+      mapOptions: {"minZoom":5,"maxZoom":10,"tileURL":"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"attribution":"<a href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\">&copy; OpenStreetMap contributors</a>"}},
+      mapLayers: [{"min_zoom":0,"max_zoom":20,"subfolder":"districts","label":"indicator.map"}],
       precision: precision,
       decimalSeparator: decimalSeparator,
     });
